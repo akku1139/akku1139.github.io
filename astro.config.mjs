81badb00ca06f500450d608c34572829b1b9ef19
-akku- <151517253+akku1139@users.noreply.github.com>
2023-12-10 09:20:31 +0000

    お試し

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config'
 
+
 export default defineConfig({
   site: 'https://akku1139.github.io',
   base: '/',
-})
+  vite: {
+    resolve: {
+      alias: {
+        '@/': '/src/',
+      }
+    }
+  }
+});
