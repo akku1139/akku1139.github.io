d370eccc13aa43edbde9d0f089aac09e4ce6d177
-akku- <151517253+akku1139@users.noreply.github.com>
2024-04-04 00:08:29 +0900

    破☆壊

--- build/loaders/markdown.ts
+++
@@ -1,9 +0,0 @@
-import { loader } from "./types";
-
-import { marked } from 'marked';
-
-const md: loader = (content) => {
-  return marked(content);
-}
-
-export default md;
--- build/loaders/types.ts
+++
@@ -1,1 +0,0 @@
-export type loader = (content: String) => String;
