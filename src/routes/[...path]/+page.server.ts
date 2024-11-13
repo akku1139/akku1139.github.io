30f34f3b9670dd1744b4d83690f1ab0ed20bba10
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-13 20:51:47 +0900

    fix

--- src/routes/[...path]/+page.server.ts
+++ src/routes/[...path]/+page.server.ts
@@ -1,6 +1,7 @@
 import { branch, leaf } from "$lib/server/hugoBundle.ts"
 
 const pages = [...branch, ...leaf]
+const routes = Object.fromEntries(pages.map(page => [page.route, page]))
 
 export const entries = () => {
   return pages.map(f => ({
@@ -10,7 +11,7 @@ export const entries = () => {
 
 export const load = async ({ params }) => {
   console.log("load:", params.path)
-  const page = await import(pages[params.path])
+  const page = await import(pages[params.path].file)
   return {
     Content: page.default,
   }
