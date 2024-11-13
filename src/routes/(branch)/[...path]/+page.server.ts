564603ec4466d899900d2a44fc697d47f9192bfc
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-13 20:12:57 +0900

    fix

--- src/routes/(branch)/[...path]/+page.server.ts
+++ src/routes/(branch)/[...path]/+page.server.ts
@@ -3,3 +3,9 @@ import { branch } from "$lib/server/HugoBundle.ts"
 export const entries = () => {
   return branch.map(f => f.route)
 }
+
+export const load = ({ params }) => {
+  return {
+    importPath: branch[params.path]
+  }
+}
--- src/routes/(leaf)/[...path]/+page.server.ts
+++ src/routes/(leaf)/[...path]/+page.server.ts
@@ -3,3 +3,9 @@ import { leaf } from "$lib/server/HugoBundle.ts"
 export const entries = () => {
   return leaf.map(f => f.route)
 }
+
+export const load = ({ params }) => {
+  return {
+    importPath: branch[params.path]
+  }
+}
