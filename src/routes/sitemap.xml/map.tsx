c0574abb402cf67097be7ea1d3eac341ecdebd51
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-15 00:11:23 +0900

    fix4

--- src/routes/sitemap.xml/map.tsx
+++ src/routes/sitemap.xml/map.tsx
@@ -2,13 +2,11 @@ import { pages } from "$lib/hugoBundle.ts"
 
 const data = await Promise.all(pages.map(async r => {
   const meta = (await r.mod()).metadata ?? {}
-  return <>
-    <loc>{ import.meta.env.VITE_BASE_URL }{ r.route }</loc>
+  return <url>
+    <loc>https://akku1139.github.io/{ r.route }</loc>
     <lastmod>{ meta.date ?? new Date().toISOString() }</lastmod>
-  </>
+  </url>
 }))
 
 export default '<?xml version="1.0" encoding="utf-8" standalone="yes"?>'
   + <urlset>{ data }</urlset>
-
-// import.meta.env.BASE_URL
