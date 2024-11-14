da5bad3f95a36b097932c686d45cbe619137e50a
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-14 23:54:51 +0900

    fix2

--- src/routes/sitemap.xml/map.tsx
+++ src/routes/sitemap.xml/map.tsx
@@ -1,12 +1,11 @@
-import { routes } from "$lib/hugoBundle.ts"
+import { pages } from "$lib/hugoBundle.ts"
 
-const data = await Promise.all(routes[""].map(async r => {
+const data = await Promise.all(pages.map(async r => {
   return <>
     <loc>https://.../{ r.route }</loc>
     <lastmod>{ (await r.mod()).metadata.date }</lastmod>
   </>
 }))
-
 
 export default '<?xml version="1.0" encoding="utf-8" standalone="yes"?>'
   + <urlset>{ data }</urlset>
