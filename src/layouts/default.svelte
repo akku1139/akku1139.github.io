be9006b0b6544d14f8406cc9b9723154978be504
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-14 21:50:05 +0900

    fix2

--- src/layouts/default.svelte
+++ src/layouts/default.svelte
@@ -2,22 +2,21 @@
 https://github.com/pngwn/MDsveX/issues/455
 https://github.com/pngwn/MDsveX/issues/287
 https://qiita.com/toshimin/items/5f13c3b4c28825219231
+https://github.com/pngwn/MDsveX/blob/f29f31c281c93a1f5e604ccc9f8e4cc14208a8d8/packages/mdsvex/test/_fixtures/LayoutThreeWithComponents.svelte
 -->
 
-<script context="module" lang="ts">
+<script module lang="ts">
   import { h1, h2 } from "./tags"
-  export * from "./tags"
+  // export * from "./tags"
 
   const md = metadata
   export const metadata = {
     ...md,
   }
 
-  console.log("md:", metadata)
-</script>
-
-<script lang="ts">
   const { children } = $props()
+
+  console.log("md:", metadata)
 </script>
 
 {@render children()}
