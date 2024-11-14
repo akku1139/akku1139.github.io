d0b21b1f1443cc7058954f7f71e9010cafa58aa0
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-15 00:23:34 +0900

    f

--- src/layouts/default.svelte
+++ src/layouts/default.svelte
@@ -10,7 +10,7 @@ https://github.com/pngwn/MDsveX/blob/f29f31c281c93a1f5e604ccc9f8e4cc14208a8d8/pa
 </script>
 
 <script module lang="ts">
-  import { h1, h2 } from "./tags.ts"
+  import { h1, h2 } from "./tags"
   export { h1, h2 }
 
   //export const metadata = {
