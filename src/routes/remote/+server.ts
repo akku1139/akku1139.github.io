edb357b6078efa2f75e3b81c39caae77822258ff
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-15 12:13:18 +0900

    remote fetch?

---
+++ src/routes/remote/+server.ts
@@ -0,0 +1,9 @@
+export const GET = ({ url }) => {
+  const target = url.searchParams.get("url")
+  if(!target) {
+    return new Response("Missing url")
+  }
+  return fetch(target)
+}
+
+// export const prerender = true
