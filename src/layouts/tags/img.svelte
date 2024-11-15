f8e701886c73b6cc3dcaa2feca9eaa39fec9c1eb
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-15 11:04:47 +0900

    debug

--- src/layouts/tags/img.svelte
+++ src/layouts/tags/img.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-  const { enhanced="true", ...props } = $props()
+  const { src, enhanced="true", ...props } = $props()
 
+  console.log("img:", src, props)
   // Since each page is small, it may be better not to do lazy loading.
 </script>
 
 {#if enhanced === "false"}
-<img {...props} />
+<img {src} {...props} />
 {:else}
-<enhanced:img {...props} />
+<enhanced:img {src} {...props} />
 {/if}
