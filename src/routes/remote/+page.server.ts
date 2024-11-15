e2c9f2bd4ed7cb2d4bc67b121087214abcb75d72
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-15 12:34:36 +0900

    a

---
+++ src/routes/remote/+page.server.ts
@@ -0,0 +1,4 @@
+export const entries = () => [
+  "https%3A%2F%2Fb.blogmura.com%2Fbanner-blogmura-landscape.svg)](https://blogmura.com/ranking/in?p_cid=11191815",
+  "https%3A%2F%2Fmoe-counter.glitch.me%2Fget%2F%40akku1139.github.io%3Ftheme%3Drule34%26darkmode%3D0"
+]
