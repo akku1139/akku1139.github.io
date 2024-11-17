6850cb5f69fed456def55412568ba5ccbc30d7bb
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-17 20:14:34 +0900

    fix

--- src/routes/[...path=branch]/index.xml/+server.ts
+++ src/routes/[...path=branch]/index.xml/+server.ts
@@ -3,6 +3,6 @@ import rss from "./rss.tsx"
 export const prerender = true
 export const trailingSlash = "never"
 
-export const GET = () => {
-  return new Response(rss)
+export const GET = async () => {
+  return new Response(await rss())
 }
