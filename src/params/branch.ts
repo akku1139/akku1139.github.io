210fdd22b664c0e3d19e03e53c1e1f754e0827cd
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-17 21:47:32 +0900

    dbg

--- src/params/branch.ts
+++ src/params/branch.ts
@@ -3,6 +3,7 @@ import { routes } from "$lib/hugoBundle.ts"
 export const match = (rawPath => {
   const path = rawPath.replace(/\/$/, "")
   const route = routes[path]
+  console.log("matcher (branch) path, route:", path, route)
   const branch = route.branch
   return branch
 })
